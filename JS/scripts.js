da6349c3d1ada4f62298ad0e2676ca27accd8c22
hyunchul and vera we <hyunchul+vera@>
2019-08-12 15:31:08 -0700

    learn jQuery

---
+++ JS/scripts.js
@@ -0,0 +1,27 @@
+var add= function(number1, number2) {
+return number1 + number2;
+
+};
+alert(add(10,2));
+var jack1 = parseInt("45");
+var jack2 = parseInt("5");
+
+alert(add(jack1,jack2));
+
+var corn1 = prompt("1");
+var corn2 = prompt("2");
+
+
+// pic walrus
+//me
+
+jQuery ("h1").click(function() {
+  alert("this is Jack");
+}
+jQuery("p").click(function() {
+  alert("what is my plan");
+}
+jQuery("image").click(function() {
+  alert("When I was young");
+
+});
